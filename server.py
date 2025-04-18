ed00332f31aea8e53d4d220520a10c90aae50e8c
s8700012 <s8700012@gmail.com>
2025-04-18 09:25:58 +0800

    server.py

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
-# 完整 Flask 伺服器將放這裡
+from flask import Flask, send_file, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_file('index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
