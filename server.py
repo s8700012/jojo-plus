48f3572bcc64da5ccdab77f3ebb3c4faffcc19a6
s8700012 <s8700012@gmail.com>
2025-04-18 08:27:44 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file, send_from_directory
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return send_file('index.html')
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
